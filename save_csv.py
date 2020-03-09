dd9f231b4dfebc26e8840ab1386666c0b0bd6e15
Stew Alexander <48565067+StewAlexanderACC@users.noreply.github.com>
2020-03-09 15:46:25 -0400

    Update save_csv.py

--- save_csv.py
+++ save_csv.py
@@ -1,11 +1,11 @@
 nlist =['$210.00','$4,150.30','$37.38']
-Nfile =open('checkbook.cvs','w')
+Nfile =open('checkbook.csv','w')
 numtotal_2d ="$4,500"
-
+# save elements to  a file separated by 
 for element in  nlist:
 	Nfile.write(element)
 	Nfile.write(',')
 Nfile.close()
 
-with open('checkbook.cvs', 'a') as Nfile:
+with open('checkbook.csv', 'a') as Nfile:
 	Nfile.write(numtotal_2d)
