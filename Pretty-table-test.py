f819dd26b6693a9a704cbdfeba47890544368958
Stew Alexander <48565067+StewAlexanderACC@users.noreply.github.com>
2020-03-20 15:31:58 -0400

    printing Python PrettyTable using a for loop

--- Pretty-table-test.py
+++ Pretty-table-test.py
@@ -1,15 +1,24 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
 # From https://ptable.readthedocs.io/en/latest/tutorial.html
 
 from prettytable import PrettyTable
+import csv
 
 t = PrettyTable()
 
-start_num = 50
-nlistmin_2d =[1,2,3,4,5]
+Org_bal = "Org Bal: "
+StartNum_2d = 50
+StartNum_Str = str(StartNum_2d)
+first_row = Org_bal + StartNum_Str
+nlistmin_2d =["$12.50",2,3,4,5]
 
-t = PrettyTable ([start_num])
-for e in nlistmin_2d:
-	t.add_row([e])
-print(t)
+t= PclerettyTable ([first_row])
+
+with open('Checkbook.txt', 'a') as f:
+	writer = csv.writer(f)
+		
+	for e in nlistmin_2d:
+		t.add_row([e])
+	writer.writerow([t])
+
